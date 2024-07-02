3dec891156234d452cbe5ad3197c19dc8a40aaa2
binumarcodes <abdulrahmanbinumar179@gmail.com>
2024-07-02 12:56:34 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Design/",
   plugins: [react()],
+  build: {
+    outDir: 'build', // Specify the output directory for the build
+  },
+  base: "/Design/",
 })
